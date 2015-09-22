5098643c0b4e83b64fb9cb25ac54d69a4e8dda90
Alonso S <alonsosalcido13@gmail.com>
2015-09-22 01:11:43 -0600

    Falta el desarrollo del formulario, y parte de la pagina principal, promociones y costos.

---
+++ js/videoBack.js
@@ -0,0 +1,21 @@
+var vid = document.getElementById("bgvid");
+var pauseButton = document.getElementById("vidpause");
+
+function vidFade(){
+	vid.classList.add("stopfade");
+}
+vid.addEventListener('ended', function(){
+	vid.pause();
+	vidFade();
+});
+
+/*pauseButton.addEventListener("click", function(){
+	vid.classList.toggle("stopfade");
+	if (vid.paused) {
+		vid.play();
+		pauseButton.innerHTML = "Pause";
+	}else{
+		vid.pause();
+		pauseButton.innerHTML = "Paused";
+	}
+});**/
